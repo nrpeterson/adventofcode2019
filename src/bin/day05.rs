9ade31eff411180463f57d6caf89a1a62d5c1ce6
Nick Peterson <nrpeterson@gmail.com>
2025-03-04 19:50:04 -0800

    Complete refactor of intcode library

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,26 +1,24 @@
 use adventofcode2019::build_main_res;
-use adventofcode2019::computer::IntcodeError::LogicError;
-use adventofcode2019::computer::{IntcodeResult, Runnable, System};
 use adventofcode2019::computer::io::{Bus, Const, Last};
+use adventofcode2019::computer::IntcodeError::ExpectedOutput;
+use adventofcode2019::computer::{IntcodeResult, Runnable, System};
 
-fn part1(input: &str) -> IntcodeResult<isize> {
-    let i = Const(1);
+fn run_with_input(code: &str, n: isize) -> IntcodeResult<isize> {
+    let i = Const(n);
     let o = Last(None);
     let io = Bus { input: i, output: o };
-    let mut system = System::parse(input, io)?;
+    let mut system = System::parse(code, io)?;
     system.run()?;
 
-    system.io.output.0.ok_or(LogicError("No output".to_string()))
+    system.io.output.0.ok_or(ExpectedOutput)
+}
+
+fn part1(input: &str) -> IntcodeResult<isize> {
+    run_with_input(input, 1)
 }
 
 fn part2(input: &str) -> IntcodeResult<isize> {
-    let i = Const(5);
-    let o = Last(None);
-    let io = Bus { input: i, output: o };
-    let mut system = System::parse(input, io)?;
-    system.run()?;
-
-    system.io.output.0.ok_or(LogicError("No output".to_string()))
+    run_with_input(input, 5)
 }
 
 build_main_res!("day05.txt", "Part 1" => part1, "Part 2" => part2);
